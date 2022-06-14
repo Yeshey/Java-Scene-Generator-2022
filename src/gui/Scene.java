7c14aa7f05e0e0398a8f19e465fdcb809de8df65
Yeshey <a2020144466@isec.pt>
2022-06-14 11:23:20 +0200

    terrain button working

--- src/gui/Scene.java
+++ src/gui/Scene.java
@@ -46,7 +46,7 @@ public class Scene extends JFrame {
     }
 
     public void setRandomColor() {
-        String[] colors = {"#5D3FD3", "#00A36C", "#088F8F"};
+        String[] colors = {"#5D3DF3", "#00A36C", "#088F8F"};
         //System.out.println(fishArrayList.get(1).getFishHex());
 
         for (Fish fish : fishArrayList) {
@@ -107,5 +107,9 @@ public class Scene extends JFrame {
     public void draw() {
         drawFishes();
         terrain.draw();
+    }
+
+    public void resetTerrain(){
+        terrain.newSeed();
     }
 }
